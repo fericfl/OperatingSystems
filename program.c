2fc8171c28b26dc96d850d8274945bec849c863f
fericfl <eric.florea@student.upt.ro>
2023-05-11 18:48:09 +0300

    "Added Directory menu and all functionality for it!"

--- program.c
+++ program.c
@@ -5,7 +5,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
 
+//COUNT C FILES INSIDE DIRECTORY
+void countCFiles(char* arg) {
+    DIR* dir;
+    struct dirent* readd;
+    int count = 0;
+    int lenght = 0;
+    dir = opendir(arg);
+    if(dir != NULL) {
+        while((readd = readdir(dir)) != NULL) {
+            lenght = strlen(readd->d_name);
+            if(strstr(readd->d_name, ".c") != NULL)
+                count++;
+        }
+        closedir(dir);
+    } else {
+        printf("Couldn't open directory");
+    }
+    printf("The total number of C files is: %d\n", count);
+}
+
+//GET SCORE FUNCTION
 void getScore(int errors, int warnings, char* arg) {
     float score = 0;
     if(errors != 0)
@@ -24,6 +47,7 @@ void getScore(int errors, int warnings, char* arg) {
     fclose(out);
 }
 
+//FUNCTION TO PRINT ACCESS RIGHT ACCORDING TO PROJECT SPECIFICATION
 void access_rights(char* arg) {
     struct stat st;
     lstat(arg, &st);
@@ -68,11 +92,16 @@ int main(int argc, char *argv[]) {
     }
     else {
         for(int i = 1; i < argc; i++) {     
-            lstat(argv[i], &st);
+            if(lstat(argv[i], &st) == -1) {
+                perror("lstat");
+                continue;
+            }
             pid_t menu = fork();
             if(menu == 0) {
+                //REGULAR FILE MENU
                 if(S_ISREG(st.st_mode) != 0) {
-                    printf("The current argument you passed is a regular file.\nWhat do you want to do with it?\n[-n] - file name\n[-d] - Dimension / size\n[-h] - Number of hard links\n[-m] - Time of last modification\n[-a] - Access rights\n[-l] - Create a symbolic link (provide the link's name)\n[-q] - Exit Program\n");
+                    fflush(stdin);
+                    printf("The current argument you passed is a regular file.\nWhat do you want to do with it?\n[-n] - file name\n[-d] - Dimension / size\n[-h] - Number of hard links\n[-m] - Time of last modification\n[-a] - Access rights\n[-l] - Create a symbolic link (provide the link's name)\n[-q] - Quit the program / Go to the next argument\n");
                     printf("Options: ");
                     int option = getchar();
                     int running = 1;
@@ -105,16 +134,17 @@ int main(int argc, char *argv[]) {
                                             printf("Couldn't create symbolic link\n");
                                         break;
                             case '-':   break;
-                            case '\n':  printf("\n\nWhat do you want to do with it?\n[-n] - file name\n[-d] - Dimension / size\n[-h] - Number of hard links\n[-m] - Time of last modification\n[-a] - Access rights\n[-l] - Create a symbolic link (provide the link's name)\n[-q] - Exit Program\nOptions:");
+                            case '\n':  printf("\n\nWhat do you want to do with it?\n[-n] - file name\n[-d] - Dimension / size\n[-h] - Number of hard links\n[-m] - Time of last modification\n[-a] - Access rights\n[-l] - Create a symbolic link (provide the link's name)\n[-q] - Quit the program / Go to next argument\nOptions:");
                                         break;   
                             default:    printf("'%c' is not a valid option in the menu!\n", option);
                                         break;
                         }
                     }
                 }
+                //SYMBOLIC LINK MENU
                 if(S_ISLNK(st.st_mode) != 0) {
                     fflush(stdin);
-                    printf("This is a link file\nWhat do you want to do with it?\n[-n] - File name\n[-l] - Delete symbolic link\n[-d] - Size of symbolic link\n[-t] - Size of target file\n[-a] - Access rights\n[-q] - Quit the program\nOptions:");
+                    printf("This is a link file\nWhat do you want to do with it?\n[-n] - File name\n[-l] - Delete symbolic link\n[-d] - Size of symbolic link\n[-t] - Size of target file\n[-a] - Access rights\n[-q] - Quit the program / Go to next argument\nOptions:");
                     int option = getchar();
                     int running = 1;
                     while((option = getchar()) != EOF && running) {
@@ -135,16 +165,47 @@ int main(int argc, char *argv[]) {
                             case 'a':   access_rights(argv[i]);
                                         break;
                             case '-':   break;
-                            case '\n':  printf("\nWhat do you want to do with it?\n[-n] - File name\n[-l] - Delete symbolic link\n[-d] - Size of symbolic link\n[-t] - Size of target file\n[-a] - Access rights\n[-q] - Quit the program\nOptions:");
+                            case '\n':  printf("\nWhat do you want to do with it?\n[-n] - File name\n[-l] - Delete symbolic link\n[-d] - Size of symbolic link\n[-t] - Size of target file\n[-a] - Access rights\n[-q] - Quit the program / Go to next argument\nOptions:");
                                         break;
                             default:    printf("'%c' is not a valid option in the menu!\n", option);
                                         break;
                         }
                     }
                 }
+                //DIRECTORY MENU
                 if(S_ISDIR(st.st_mode) != 0) {
+                    fflush(stdin);
                     printf("This is a directory!\n");
-                    printf("Hey\n");
+                    printf("What do you want to do with it?\n[-n] - Directory name\n[-d] - Size of directory\n[-a] - Access rights\n[-c] - Total number of files with the .c extension\n[-q] - Quit program / Go to next argument\nOptions:");
+                    int running = 1;
+                    int option;
+                    while((option = getchar()) != EOF && running) {
+                        switch (option)
+                        {
+                        case 'n':
+                            printf("\nDirectory name is: %s", argv[i]);
+                            break;
+                        case 'd':
+                            printf("\nSize of the directory is: %ld", st.st_size);
+                            break;
+                        case 'a':
+                            access_rights(argv[i]);
+                            break;
+                        case 'c':
+                            countCFiles(argv[i]);
+                            break;
+                        case 'q':
+                            running = 0;
+                            break;
+                        case '-': break;
+                        case '\n': 
+                            printf("\nWhat do you want to do with it?\n[-n] - Directory name\n[-d] - Size of directory\n[-a] - Access rights\n[-c] - Total number of files with the .c extension\n[-q] - Quit program / Go to next argument\nOptions:");
+                            break;
+                        default:
+                            printf("\n'%c' is not a valid option in the menu!", option);
+                            break;
+                        }
+                    }
                 }
             } else if(menu > 0) {
                 int status;
@@ -154,7 +215,7 @@ int main(int argc, char *argv[]) {
                     perror("Second child process could not be created");
                     exit(1);
                 } else if(s_child == 0) {
-                    if(strstr(argv[i], ".c") != NULL) {
+                    if((strstr(argv[i], ".c") != NULL)) {
                         int pfd[2]; //[0] - read, [1] - write
                         int newfd;
                         if(pipe(pfd) < 0) {
@@ -185,7 +246,8 @@ int main(int argc, char *argv[]) {
                         int warnings = atoi(string);
                         getScore(errors, warnings, argv[i]);
                         close(pfd[0]);
-                    } else {
+                    }
+                    if(S_ISREG(st.st_mode) != 0) {
                         int pfd[2]; //[0] - read, [1] - write
                         int newfd;
                         if(pipe(pfd) < 0) {
@@ -213,8 +275,34 @@ int main(int argc, char *argv[]) {
                         char string[100];
                         fscanf(stream, "%s", string);
                         int word_count = atoi(string);
-                        printf("Total lines: %d\n", word_count);
+                        printf("Total lines of text: %d\n", word_count);
                         close(pfd[0]);
+                    } 
+                    if(S_ISLNK(st.st_mode) != 0) {
+                        printf("Changing permissions for symbolic link %s\n", argv[i]);
+                        execlp("chmod", "chmod", "-h", "u=rwx,g=rw,o=", argv[i], NULL);
+                        perror("execlp");
+                        exit(1);   
+    
+                    }
+                    if(S_ISDIR(st.st_mode) != 0) {
+                        char file_name[strlen(argv[i]) + 11];
+                        if(strrchr(argv[i], '/') != 0) {
+                            char* dir_name = strrchr(argv[i], '/') + 1;
+                            sprintf(file_name, "%s/%s_file.txt", argv[i], dir_name);
+                        } else {
+                            strcpy(file_name, argv[i]);
+                            strcat(file_name, "/");
+                            strcat(file_name, argv[i]);
+                            strcat(file_name, "_file.txt");
+                        }
+                        int fd = open(file_name, O_WRONLY | O_CREAT, 0644);
+                        if(fd == -1) {
+                            perror("open");
+                            exit(EXIT_FAILURE);
+                        }
+                        close(fd);
+                        exit(EXIT_SUCCESS);
                     }
                 } else {
                     int status;
